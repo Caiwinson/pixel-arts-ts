9c12a5f61cfc15eb46cee6093949eee47f23e7f5
Caiwinson <caiwinson06@gmail.com>
2026-03-04 05:02:54 +0700

    refactor: app_mode

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,43 @@ import { DISCORD_TOKEN } from "./constants.js";
 import app from "./web/server.js";
 import { initDb } from "./database.js";
 
+const MODE = process.env.APP_MODE;
+
 initDb();
 
-app.set("trust proxy", 1);
-app.listen(8080, () => {
-    console.log("🌐 Web server listening on port 8080");
-});
+async function startWeb() {
+    app.set("trust proxy", 1);
 
-const manager = new ShardingManager("./dist/bot/bot.js", {
-    token: DISCORD_TOKEN!,
-    totalShards: "auto",
-});
+    app.listen(8080, () => {
+        console.log("🌐 Web server listening on port 8080");
+    });
+}
 
-manager.on("shardCreate", (shard) => {
-    console.log(`🚀 Shard ${shard.id} launched`);
-});
+async function startBot() {
+    const manager = new ShardingManager("./dist/bot/bot.js", {
+        token: DISCORD_TOKEN!,
+        totalShards: "auto",
+    });
 
-manager.spawn();
+    manager.on("shardCreate", (shard) => {
+        console.log(`🚀 Shard ${shard.id} launched`);
+    });
+
+    await manager.spawn();
+}
+
+switch (MODE) {
+    case "web":
+        console.log("Starting WEB mode");
+        startWeb();
+        break;
+
+    case "bot":
+        console.log("Starting BOT mode");
+        startBot();
+        break;
+
+    default:
+        console.error("❌ APP_MODE must be 'web' or 'bot'");
+        process.exit(1);
+}
